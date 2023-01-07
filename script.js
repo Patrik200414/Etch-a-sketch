7fb8cedcaaf70244b005400775f4e3ab61a9ae10
Patrik200414 <martinpatrik14@gmail.com>
2023-01-07 19:53:49 +0100

    Adding an id generator function and set the ids for the td elements

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let state = {
-    rowsAndCollumns : 16
+    rowsAndCollumns : 16,
+    currentColor: '#000000'
 };
 
 
 
 document.querySelector('#slider').addEventListener('change', (event) => {
     state.rowsAndCollumns = event.target.value;
+    document.querySelector('.size').textContent = `${event.target.value} X ${event.target.value}`
     RenderSquares();
 })
 
+document.querySelector('#drawingContainer').addEventListener('click', (event) => {
+    console.log(event.target.id)
+})
 
 
 function RenderSquares(){
@@ -21,6 +26,7 @@ function RenderSquares(){
         for(let j= 0;j<state.rowsAndCollumns;j++){
             let td = document.createElement('td');
             td.setAttribute('class', 'styleSquare');
+            td.setAttribute('id', uniqueid());
             tr.append(td)
         }
         table.append(tr);
@@ -36,3 +42,21 @@ function RenderSquares(){
 }
 
 RenderSquares();
+
+
+
+
+function uniqueid(){
+    // always start with a letter (for DOM friendlyness)
+    var idstr=String.fromCharCode(Math.floor((Math.random()*25)+65));
+    do {                
+        // between numbers and characters (48 is 0 and 90 is Z (42-48 = 90)
+        var ascicode=Math.floor((Math.random()*42)+48);
+        if (ascicode<58 || ascicode>64){
+            // exclude all chars between : (58) and @ (64)
+            idstr+=String.fromCharCode(ascicode);    
+        }                
+    } while (idstr.length<32);
+
+    return (idstr);
+}
