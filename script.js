b38ae876389de39811b5fdc3625da1021654bfd7
Patrik200414 <martinpatrik14@gmail.com>
2023-01-08 11:24:12 +0100

    Only color when the alt key is pressed
    It only hange the color when the alt key is pressed

--- script.js
+++ script.js
@@ -60,23 +60,25 @@ document.querySelector('#erase').addEventListener('click', (event) => {
 
 
 
-document.querySelector('#drawingContainer').addEventListener('click', (event) => {
-    if(state.mode === 'color'){
-        if(event.target.className !== 'drawing-container'){
-            state.currentColor = '#000000';
-            document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
+document.querySelector('#drawingContainer').addEventListener('mouseover', (event) => {
+    if(event.altKey){
+        if(state.mode === 'color'){
+            if(event.target.className !== 'drawing-container'){
+                state.currentColor = '#000000';
+                document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
+            }
+            
+        }else if(state.mode === 'rainbow'){
+            if(event.target.className !== 'drawing-container'){
+                Rainbow();
+                document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
+            }
         }
-        
-    }else if(state.mode === 'rainbow'){
-        if(event.target.className !== 'drawing-container'){
-            Rainbow();
-            document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
-        }
-    }
-    else if(state.mode === 'erase'){
-        if(event.target.className !== 'drawing-container'){
-            state.currentColor = '#ffffff';
-            document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
+        else if(state.mode === 'erase'){
+            if(event.target.className !== 'drawing-container'){
+                state.currentColor = '#ffffff';
+                document.getElementById(event.target.id).style.backgroundColor = state.currentColor;
+            }
         }
     }
     
